40eb05cc51e989e27236ad13e95f8f8f015ccbda
sergeyterekhin <chupakabra228228@gmail.com>
2021-09-23 19:28:28 +0700

    ENV AND CONFIG

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,7 +5,13 @@ Class HomeController extends Controller
 {
     public function index()
     {
-
+        //env() получает настройки из группы настроек окружения
+        //config() настройки самого приложения
+        //cчитывает настройки среды
+        dump($_ENV['MY_SETTING']);
+        dump(env('MY_SETTING2','not found'));
+        dump(config('app.timezone'));
+        dump(config('database.connections.mysql.database'));
         return view('home',['res'=>5,'name'=>'John']);
     }
     public function test(){
